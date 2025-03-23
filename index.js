d91bf4ccd8c57d7884816a047c14ffb13dc475b3
Nari-16 <narirobert16@gmail.com>
2025-03-23 19:43:58 +0100

    add index.html file

---
+++ index.js
@@ -0,0 +1,22 @@
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+let previousEntries = []
+
+let count = 0
+
+function increment () {
+    count = count + 1
+    countEl.innerText = count
+}
+function save() {
+    previousEntries.push(count);
+    document.getElementById("previous-entries").textContent = previousEntries.join(" - ");
+    count = 0;
+    document.getElementById("count-el").textContent = count;
+    console.log(count)
+}
+let message = "you have logged"
+console.log(messageToUser)
+
+
